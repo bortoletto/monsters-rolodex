e28663cf94c28a9c7274387c274de9df04adb7dd
Albert Rocha Bortoletto <albert.bortoletto@hotmail.com>
2019-08-19 00:12:01 -0300

    Updates

---
+++ precache-manifest.62c72d4309abb218245d94be8fe9d820.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8c5bede788ad7369a054bdc31de4715",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b933fa37c47d4afa677e",
+    "url": "/monsters-rolodex/static/css/main.2a9a8b4c.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "b933fa37c47d4afa677e",
+    "url": "/monsters-rolodex/static/js/main.932155ba.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
